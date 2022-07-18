9ce7ced0aa5be5c8b18f488f70df1a692b20c851
sourov <sourovbm21@gmail.com>
2022-07-18 11:51:56 +0100

    adding file

---
+++ src/main/java/QA01/GitA/App.java
@@ -0,0 +1,18 @@
+package QA01.GitA;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        System.out.println("in side the Git--A");
+        
+        
+        
+    }
+}
