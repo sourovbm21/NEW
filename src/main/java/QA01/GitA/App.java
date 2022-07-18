de00b5b93ba0d747dd2416aa510031a1756a7941
sourovbm21 <69170140+sourovbm21@users.noreply.github.com>
2022-07-18 11:53:21 +0100

    addin manually

--- src/main/java/QA01/GitA/App.java
+++ src/main/java/QA01/GitA/App.java
@@ -11,7 +11,7 @@ public class App
         System.out.println( "Hello World!" );
         
         System.out.println("in side the Git--A");
-        
+        System.out.println("adding manually");
         
         
     }
