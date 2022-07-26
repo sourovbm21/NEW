732ee15632f6759cfee26301a68e4985c4a4c6af
sourov <sourovbm21@gmail.com>
2022-07-26 13:38:50 +0100

    a

---
+++ src/main/java/QA01/GitA/WithNG.java
@@ -0,0 +1,31 @@
+package QA01.GitA;
+
+import org.testng.annotations.Test;
+
+public class WithNG {
+	
+	
+	
+	
+	@Test 
+	public void testOne() {
+		System.out.println("i am inside test one Method");
+	}
+	
+	
+	@Test 
+	public void testTwo() {
+		System.out.println("i am inside testTwo Method");
+	}
+	@Test 
+	public void testThree() {
+		System.out.println("i am inside testThree Method");
+	}
+	@Test 
+	public void testFour() {
+		System.out.println("i am inside testFour Method");
+	System.out.println();
+    
+	}
+
+}
